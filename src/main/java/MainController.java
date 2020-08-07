c3a1cf8a59ad89eeb5d3b9e0468cfc7af06b15b2
Isaac Scarisbrick <isasca@hotmail.co.uk>
2020-08-07 18:07:43 +0100

    Added initialization function in MainController.

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -2,15 +2,23 @@ import com.brunomnsilva.smartgraph.graphview.SmartGraphPanel;
 import javafx.fxml.FXML;
 import javafx.scene.SubScene;
 
+import javax.annotation.Resources;
+import java.net.URL;
+
 
 public class MainController {
   
   @FXML SubScene graphScene;
-  //SmartGraphPanel<String, String> graphPanel= (SmartGraphPanel<String, String>) graphScene.getRoot();
+  SmartGraphPanel<String, String> graphPanel;
   
   @FXML
   private void generateGraph() {
     System.out.println(graphScene.getRoot());
+  }
+  
+  @FXML
+  protected void initialize(URL location, Resources resources) {
+    graphPanel = (SmartGraphPanel<String, String>) graphScene.getRoot();
   }
   
 }
